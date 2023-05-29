860f1348066cb85e03f9719e6597117f081574e1
gomao <gboycdw@gmail.com>
2023-05-30 04:35:46 +0900

    [feat] 팀/팀원 구인 페이지 Route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import TeamPage from './Components/Teampage/TeamPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
+        <Route path="/teampage" element={<TeamPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
