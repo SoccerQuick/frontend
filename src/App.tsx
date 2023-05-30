8eac63d2579a3970de73c512c27085574691ecbe
minu <wlwhsxz@gmail.com>
2023-05-30 22:24:21 +0900

    [Feat] 메인 페이지 BodyUpperContainer 반응형 css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import * as React from 'react';
-import logo from './logo.svg';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TeamPage from './Components/Teampage/TeamPage';
+
+import Main from './Pages/Main';
+import Reviews from './Pages/Reviews';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/teampage" element={<TeamPage />}></Route>
+        <Route path="/" element={<Main />} />
+        <Route path="/reviews" element={<Reviews />} />
       </Routes>
     </BrowserRouter>
   );
