0a037e4e68168dee4173857c0234db5f86d66e8c
minu <wlwhsxz@gmail.com>
2023-05-30 22:30:11 +0900

    [Feat] 메인 페이지 BodyUpperContainer 반응형 css 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TeamPage from './Components/Teampage/TeamPage';
-
 import Main from './Pages/Main';
 import Reviews from './Pages/Reviews';
 
---
+++ src/Pages/Reviews.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Reviews() {
+  return (
+    <>
+      <h1>review page</h1>
+    </>
+  );
+}
