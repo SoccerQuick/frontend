cc81322a4f2e2c09a06ed44d6adfc3ee2fef4014
gomao <gboycdw@gmail.com>
2023-05-31 01:36:22 +0900

    [fix] 깃충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ReviewPage from './Pages/Review';
 import Login from './Components/LoginPage/Login';
 import Register from './Components/RegisterPage/Register';
 import TeamPage from './Pages/TeamPage';
-import Main from './Pages/main';
+import Main from './Pages/mainlayout';
 
 function App() {
   return (
@@ -18,7 +18,6 @@ function App() {
         <Route path="/teampage" element={<TeamPage />}></Route>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/review" element={<ReviewPage />}></Route>
-
       </Routes>
     </BrowserRouter>
   );
