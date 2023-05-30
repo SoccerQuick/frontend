bd2581effae9d9e88151c9b28c5506e30c03e400
gomao <gboycdw@gmail.com>
2023-05-30 22:34:31 +0900

    [fix] 깃 충돌 해결(올라오지 않은 파일 import 삭제)

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TeamPage from './Pages/TeamPage';
 
-import Main from './Pages/Main';
-import Reviews from './Pages/Reviews';
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/teampage" element={<TeamPage />}></Route>
-        <Route path="/" element={<Main />} />
-        <Route path="/reviews" element={<Reviews />} />
       </Routes>
     </BrowserRouter>
   );
