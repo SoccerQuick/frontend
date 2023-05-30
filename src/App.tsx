c08a5d3e8369b6ad1c03e699b509b324d54cca78
minu <wlwhsxz@gmail.com>
2023-05-30 22:46:16 +0900

    [Fix] App.tsx conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TeamPage from './Pages/TeamPage';
+import MainPage from './Pages/Main';
+import ReviewPage from './Pages/Review';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/review" element={<ReviewPage />}></Route>
         <Route path="/teampage" element={<TeamPage />}></Route>
       </Routes>
     </BrowserRouter>
