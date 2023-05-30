ebb9b81e0eeff57a6c00a092ef931d4dbe163507
gomao <gboycdw@gmail.com>
2023-05-31 07:12:46 +0900

    [feat] 관리자 메인페이지, 팀관리페이지 틀 추가

--- src/Components/AdminPage/Contents/AdminMainPage.tsx
+++ src/Components/AdminPage/Contents/AdminMainPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function AdminMain() {
   return <div>관리자 메인페이지</div>;
--- src/Components/AdminPage/Contents/AdminTeamManagePage.tsx
+++ src/Components/AdminPage/Contents/AdminTeamManagePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function AdminTeamManager() {
+function AdminTeamManager(props: any) {
   return <div>팀 관리 페이지</div>;
 }
 
