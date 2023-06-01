8a7e7f122e4a5a73aa56aaa951de12bed3d2e077
gomao <gboycdw@gmail.com>
2023-06-02 06:55:24 +0900

    [docs] 팀 모집 페이지 상세조회 페이지 UI 작성

--- src/Components/TeamPage/Contents/FindingTeamDetail.tsx
+++ src/Components/TeamPage/Contents/FindingTeamDetail.tsx
@@ -5,10 +5,38 @@ function FindingTeamDetail(props: any) {
   const modalData = props.modalData;
   return (
     <>
-      <div>포지션 : {modalData.position}</div>
-      <div>실력 : {modalData.position}</div>
-      <div>성별 : {modalData.gender}</div>
-      <div>{modalData.body}</div>
+      <StyledDiv>
+        <div style={{ padding: '0rem 2rem' }}>작성자 정보</div>
+        <div>
+          <div
+            style={{
+              display: 'flex',
+              margin: '0.5rem 1rem',
+            }}
+          >
+            포지션 : {modalData.position}
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              margin: '0.5rem 1rem',
+            }}
+          >
+            실력 : {modalData.skill}
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              margin: '0.5rem 1rem',
+            }}
+          >
+            성별 : {modalData.gender}
+          </div>
+        </div>
+      </StyledDiv>
+      <StyledBodyContainer>
+        <StyledBody>{modalData.body}</StyledBody>
+      </StyledBodyContainer>
     </>
   );
 }
@@ -25,3 +53,41 @@ function FindingTeamDetail(props: any) {
 //   },
 
 export default FindingTeamDetail;
+
+const StyledDiv = styled.div`
+  z-index: 902;
+  display: flex;
+  align-items: center;
+  //   justify-content: space-between;
+  font-size: 1.8rem;
+  position: absolute;
+  top: 0;
+  margin-top: 15rem;
+  width: 90%;
+  height: 20%;
+  border: 1px solid #dddddd;
+  border-radius: 1rem;
+`;
+
+const StyledBodyContainer = styled.div`
+  z-index: 902;
+  display: flex;
+  //   align-items: center;
+  //   justify-content: space-between;
+  font-size: 1.8rem;
+  position: absolute;
+  top: 0;
+  margin-top: 31rem;
+  width: 90%;
+  height: 30%;
+  border: 1px solid #dddddd;
+  border-radius: 1rem;
+`;
+
+const StyledBody = styled.div`
+  z-index: 902;
+  display: flex;
+  font-size: 1.8rem;
+  margin: 1rem 1rem;
+  padding: 1rem 1rem;
+`;
