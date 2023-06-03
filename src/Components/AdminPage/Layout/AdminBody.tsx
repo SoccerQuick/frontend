bd1b5ee34422188237b00976f0147ffbacd0d77f
gomao <gboycdw@gmail.com>
2023-06-03 19:29:45 +0900

    [docs] 페이지 렌더링 방식 변경으로 컴포넌트 일시 미사용 처리

--- src/Components/AdminPage/Layout/AdminBody.tsx
+++ src/Components/AdminPage/Layout/AdminBody.tsx
@@ -1,59 +1,64 @@
 import React from 'react';
-import styled from 'styled-components';
-import AdminTeamManager from '../Contents/AdminTeamManagePage';
-import AdminUserManager from '../Contents/AdminUserManagePage';
-import AdminMain from '../Contents/AdminMainPage';
-import AdminModal from './AdminModal';
-type props = {
-  mainButton: boolean;
-  setMainButton: React.Dispatch<React.SetStateAction<boolean>>;
-  userButton: boolean;
-  setUserButton: React.Dispatch<React.SetStateAction<boolean>>;
-  teamButton: boolean;
-  setTeamButton: React.Dispatch<React.SetStateAction<boolean>>;
-  showModal: boolean;
-  setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
-  modalData: any[];
-  setModalData: React.Dispatch<React.SetStateAction<any[]>>;
-};
+// import styled from 'styled-components';
+// import AdminTeamManager from '../Contents/AdminTeamManagePage';
+// import AdminUserManager from '../Contents/AdminUserManagePage';
+// import AdminMain from '../Contents/AdminMainPage';
+// import AdminModal from './AdminModal';
+// type props = {
+//   mainButton: boolean;
+//   setMainButton: React.Dispatch<React.SetStateAction<boolean>>;
+//   userButton: boolean;
+//   setUserButton: React.Dispatch<React.SetStateAction<boolean>>;
+//   teamButton: boolean;
+//   setTeamButton: React.Dispatch<React.SetStateAction<boolean>>;
+//   showModal: boolean;
+//   setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+//   modalData: any[];
+//   setModalData: React.Dispatch<React.SetStateAction<any[]>>;
+// };
 
-function AdminBody(props: props) {
-  return (
-    <>
-      <BodyContainer>
-        {props.mainButton && <AdminMain />}
-        {props.userButton && (
-          <AdminUserManager
-            showModal={props.showModal}
-            setShowModal={props.setShowModal}
-            modalData={props.modalData}
-            setModalData={props.setModalData}
-          />
-        )}
-        {props.teamButton && (
-          <AdminTeamManager
-            showModal={props.showModal}
-            setShowModal={props.setShowModal}
-            modalData={props.modalData}
-            setModalData={props.setModalData}
-          />
-        )}
-        {props.showModal && (
-          <AdminModal
-            showModal={props.showModal}
-            setShowModal={props.setShowModal}
-            modalData={props.modalData}
-            setModalData={props.setModalData}
-          />
-        )}
-      </BodyContainer>
-    </>
-  );
-}
+// function AdminBody(props: props) {
+//   return (
+//     <BodyMain>
+//       <BodyContainer>
+//         {props.mainButton && <AdminMain />}
+//         {props.userButton && (
+//           <AdminUserManager
+//             showModal={props.showModal}
+//             setShowModal={props.setShowModal}
+//             modalData={props.modalData}
+//             setModalData={props.setModalData}
+//           />
+//         )}
+//         {props.teamButton && (
+//           <AdminTeamManager
+//             showModal={props.showModal}
+//             setShowModal={props.setShowModal}
+//             modalData={props.modalData}
+//             setModalData={props.setModalData}
+//           />
+//         )}
+//         {props.showModal && (
+//           <AdminModal
+//             showModal={props.showModal}
+//             setShowModal={props.setShowModal}
+//             modalData={props.modalData}
+//             setModalData={props.setModalData}
+//           />
+//         )}
+//       </BodyContainer>
+//     </BodyMain>
+//   );
+// }
 
-export default AdminBody;
+// export default AdminBody;
 
-const BodyContainer = styled.div`
-  padding: 2rem 2rem;
-  height: 100%;
-`;
+// const BodyMain = styled.div`
+//   background-color: rgb(245, 245, 245);
+//   height: 80rem;
+// `;
+
+// const BodyContainer = styled.div`
+//   padding: 2rem 2rem;
+//   height: 100%;
+// `;
