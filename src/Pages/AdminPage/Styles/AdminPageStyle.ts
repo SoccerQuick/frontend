025b62bca3eb2649d9e4918df9b83b014148a0ab
gomao <gboycdw@gmail.com>
2023-06-16 06:37:01 +0900

    [refactor, fix] 관리자페이지 페이지네이션 컴포넌트 사용 및 하단 바 위치 고정

--- src/Pages/AdminPage/Styles/AdminPageStyle.ts
+++ src/Pages/AdminPage/Styles/AdminPageStyle.ts
@@ -130,6 +130,7 @@ export const PageSelect = styled.div`
   background-color: rgb(245, 245, 245);
 `;
 
+// 페이지네이션 컴포넌트를 사용하면서 사용하지 않게 된 부분
 export const PageButton = styled.button<{
   selected: number;
   currentPage: number;
